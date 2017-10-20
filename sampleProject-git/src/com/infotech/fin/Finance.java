f9dea23847584837cba170cbe2212aa8b81ab643
dell <sree.kironmai@gmail.com>
2017-10-20 19:12:25 +0530

    an Finance interface is added

---
+++ sampleProject-git/src/com/infotech/fin/Finance.java
@@ -0,0 +1,9 @@
+package com.infotech.fin;
+
+public interface Finance {
+	public void annualPremium();
+	public void halfYearPremium();
+	public void quaterlyPremium();
+	
+
+}
--- sampleProject-git/src/com/infotech/fin/InfoTest.java
+++ sampleProject-git/src/com/infotech/fin/InfoTest.java
@@ -1,10 +1,28 @@
 package com.infotech.fin;
 
-public class InfoTest {
+public class InfoTest implements Finance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("testing git project");
+	}
+
+	@Override
+	public void annualPremium() {
+		// TODO Auto-generated method stub
+		System.out.println("annual premium to be paid");	
+	}
+
+	@Override
+	public void halfYearPremium() {
+		// TODO Auto-generated method stub
+		System.out.println("half yearly");	
+	}
+
+	@Override
+	public void quaterlyPremium() {
+		// TODO Auto-generated method stub
+		System.out.println("quaterly");
 	}
 
 }
