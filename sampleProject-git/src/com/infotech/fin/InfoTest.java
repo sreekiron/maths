bed7c7c6b282696b231586cef3fe366704dabc12
dell <sree.kironmai@gmail.com>
2017-10-20 18:52:04 +0530

    testing second project to commit in repository
    Signed-off-by: dell <sree.kironmai@gmail.com>

---
+++ sampleProject-git/src/com/infotech/fin/InfoTest.java
@@ -0,0 +1,10 @@
+package com.infotech.fin;
+
+public class InfoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("testing git project");
+	}
+
+}
